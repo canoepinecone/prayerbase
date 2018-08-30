ce41c98e12f56f7d06cc0de84660cdd0b8e97957
Melinda Swartzfager <canoepinecone@gmail.com>
2018-08-29 18:31:03 -0700

    Working on authentication and fixed app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 class Prayer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -69,4 +71,6 @@ def deleteprayer():
     db.session.commit()
     return redirect(url_for('index'))
 
-app.run()
+if __name__ == '__main__':
+    app.run()
+
